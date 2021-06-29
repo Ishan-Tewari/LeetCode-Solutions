c17feebb693527af4f78a4b2a61c2e43fab8aeda
Ishan Tewari <ishantewari92@gmail.com>
2021-06-29 17:18:54 +0530

    Added "Max Consecutive Ones III" Solution

---
+++ 29 June 2021/Solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int longestOnes(int[] nums, int k) {
+        int start = 0;
+        int end = 0;
+
+        while (end < nums.length){
+
+            if (nums[end] == 0) k--;
+
+            if (k < 0){
+
+                if (nums[start] == 0) k++;
+
+                start++;
+            }
+
+            end++;
+        }
+
+        return end - start;
+    }
+}
